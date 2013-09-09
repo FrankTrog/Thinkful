ab0fbfa8961f8a2aea4a344f0131b7c851f8f572
FrankTrog <frank@frankt3d.com>
2013-09-09 09:32:53 -0400

    Minor changes to finish project

--- CaseyWilson/HotCold/application.js
+++ CaseyWilson/HotCold/application.js
@@ -7,28 +7,6 @@ $("numberField").keyup(function(event){
     }
 });
 
-//attempt to make the gauge arrow rotate 45 degrees back and forth  
-$(function() {
-    var $gaugeArrow = $("#right-arrow"), degree = 45, slow;
-	$gaugeArrow.css({ WebkitTransform: 'rotate(' + degree + 'deg)'});
-	$gaugeArrow.css({ '-moz-transform': 'rotate(' + degree + 'deg)'});
-/*
-    function rotate() {             
-        $gaugeArrow.css({ WebkitTransform: 'rotate(' + degree + 'deg)'});        
-        $gaugeArrow.css({ '-moz-transform': 'rotate(' + degree + 'deg)'});     
-        timer = setTimeout(function() {
-            ++degree; rotate();
-        },5);
-    }
-    rotate();
-
-    $(this).toggle(function() {
-        clearTimeout(timer);
-    }, function() {
-        rotate();
-    });
-*/
-}); 
 var degrees = new Object();
 var current = null;
 var previous = null;
@@ -43,23 +21,29 @@ function submit(){
 	previous = current;
 	current = val;
 	
-	//Subtract current guess from random number and ensure the result is positive. 
-	//Divide that result by the random number and then multiply it by 100 to get a whole number. 
-	//Subtract 100 by that result and you now have the current percentage to the random number.
-	degrees.percent = 100-((Math.abs(randomNumber - current)/randomNumber)*100);
+	if(randomNumber > current){
+		//If randomNumber is greater than the guess then divide the guess by the randomNumber and multiply it by 100 to get a whole number
+		degrees.percent = (current/randomNumber)*100;	
+	}else
+		//If randomNumber is less than the guess then divide the randomNumber by the guess and multiply it by 100 to get a whole number
+		degrees.percent = (randomNumber/current)*100;
 		
 	degrees.cold = 45;
-	degrees.hot = 67;
+	degrees.hot = 67; //Was 67
 	degrees.hotperc = 2.2;
 	degrees.coldperc = 1.78
 	
-	if(degrees.percent == 56){
+	//The best way to think of this is to think of the quartesian coordinate system. 0 separates the top right and bottom right sections of the graph
+	//If the precent is 50 then degrees should be 0.
+	//If percent is greater than 50 we need to divide it by 100 to get a decimal value to then multiply by the max degrees we want to go. Since we are going from 360 counter clockwise we have to subtract this number from 360
+	//If the percent is less than 50 we need to subtract 50 from the percentage, divide it by 100 and then multiply it by the max degrees we want to go. Keep in mind we are going in a positive direction clockwise from 0 down to C.
+	if(degrees.percent == 50)
 		degree = degrees.current = 0;
-	}else if(degrees.percent > 56){
-		degree = degrees.current = 360-((((degrees.percent-56)*degrees.hotperc)/100)*degrees.hot);
-	}else if(degrees.percent < 56){
-		degree = degrees.current = (((56-degrees.percent)*degrees.coldperc)/100)*degrees.cold;
-	}
+	else if(degrees.percent > 50)
+		degree = degrees.current = 360-((degrees.percent/100)*67);
+	else if(degrees.percent < 50)
+		degree = degrees.current = ((50-degrees.percent)/100)*67;
+
 	console.log(degrees.percent, degree);
 	arrow.css({ WebkitTransform: 'rotate(' + degree + 'deg)'});
     arrow.css({ '-moz-transform': 'rotate(' + degree + 'deg)'});
@@ -76,29 +60,18 @@ function submit(){
 			document.getElementById("out-of-range").style.display='none';
 		};
 
-	var removeDial = function () {
-		//document.getElementById("right-arrow").style.display = 'none';
-		//document.getElementById("cold-arrow").style.display = 'none';
-		//document.getElementById("hot-arrow").style.display = 'none';
-	};
-
 	if (val > 100 || val < 1) {
 		removeMessages();
-		removeDial();
 		document.getElementById("out-of-range").style.display='block';
-		//document.getElementById("cold-arrow").style.display = 'inline-block';
 	}
 	
 	else if (val == randomNumber) {
 		removeMessages();
-		removeDial();
 		document.getElementById("Right").style.display='block';
-		//document.getElementById("hot-arrow").style.display = 'inline-block';
 	}
 	
 	else if (current > randomNumber) {
 		removeMessages();
-		removeDial();
 		
 		if(previous !== null && Math.abs(randomNumber-current)<Math.abs(randomNumber-previous)){
 			document.getElementById("tooHigh-warmer").style.display='block';
@@ -116,18 +89,14 @@ function submit(){
 	
 	else if (current < randomNumber) {
 		removeMessages();
-		removeDial();
 		if(previous !== null && Math.abs(randomNumber-current)<Math.abs(randomNumber-previous)){
 			document.getElementById("tooLow-warmer").style.display='block';
-			//document.getElementById("right-arrow").style.display = 'inline-block';
 		}
 		else if (previous !== null && Math.abs(randomNumber-current)>Math.abs(randomNumber-previous)){
 			document.getElementById("tooLow-colder").style.display='block';
-			//document.getElementById("cold-arrow").style.display = 'inline-block';
 		}
 		else{
 			document.getElementById("tooLow").style.display='block';
-			//document.getElementById("cold-arrow").style.display = 'inline-block';
 		}
 	}
 	
@@ -135,11 +104,9 @@ function submit(){
 		removeMessages();
 		removeDial();
 		document.getElementById("NaN").style.display='block';
-		//document.getElementById("cold-arrow").style.display = 'inline-block';
 	}
 
-//erase value in text entry box
-
+	//erase value in text entry box
 	document.getElementById("numberField").value= '';
 
 };	
